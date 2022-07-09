eea066f2791c38aeb2ec70d4c254b64b7f28d52d
surajkumar076 <108928146+surajkumar076@users.noreply.github.com>
2022-07-10 00:59:17 +0530

    Create Time_Delta.py

---
+++ Time_Delta.py
@@ -0,0 +1,43 @@
+'''
+Input
+2
+Sun 10 May 2015 13:54:36 -0700
+Sun 10 May 2015 13:54:36 -0000
+Sat 02 May 2015 19:54:36 +0530
+Fri 01 May 2015 13:54:36 -0000
+
+Output
+25200
+88200
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from datetime import datetime
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    t1 = datetime.strptime(t1, '%a %d %b %Y %H:%M:%S %z')
+    t2 = datetime.strptime(t2, '%a %d %b %Y %H:%M:%S %z')
+    return str(int(abs((t1-t2).total_seconds())))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
