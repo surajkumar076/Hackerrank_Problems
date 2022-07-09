8d0b6929353ce3d76d2f720630a635203cb6818c
surajkumar076 <108928146+surajkumar076@users.noreply.github.com>
2022-07-10 00:44:21 +0530

    Create py_collections_namedtuple.py

---
+++ py_collections_namedtuple.py
@@ -0,0 +1,25 @@
+'''
+Input 
+5
+ID         MARKS      NAME       CLASS
+1          97         Raymond    7
+2          50         Steven     4
+3          91         Adrian     9
+4          72         Stewart    5
+5          80         Peter      6
+
+Output
+78.00
+'''
+from collections import namedtuple
+
+N = int(input())
+fields = input().split()
+
+total = 0
+for i in range(N):
+    students = namedtuple('student',fields)
+    field1, field2, field3,field4 = input().split()
+    student = students(field1,field2,field3,field4)
+    total += int(student.MARKS)
+print('{:.2f}'.format(total/N))
