43a7b88376dbef286c7defd6d7ecd0c50d27c0e3
Suraj_Kumar_Diggibyte <108928146+surajkumar076@users.noreply.github.com>
2022-07-13 13:45:20 +0530

    Create Word_Order.py

---
+++ Word_Order.py
@@ -0,0 +1,29 @@
+'''
+Input
+4
+bcdef
+abcdefg
+bcde
+bcdef
+
+Output
+3
+2 1 1
+'''
+
+import collections;
+
+N = int(input())
+d = collections.OrderedDict()
+
+for i in range(N):
+    word = input()
+    if word in d:
+        d[word] +=1
+    else:
+        d[word] = 1
+
+print(len(d));
+
+for k,v in d.items():
+    print(v,end = " ");
